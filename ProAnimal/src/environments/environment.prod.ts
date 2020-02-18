fe183f9f5ec308a87941593572ffa135799c87e2
BCoaracy <bruno.coaracy@gmail.com>
2020-02-18 13:43:21 -0300

    Criado a configuracao do Firebase

--- ProAnimal/src/environments/environment.prod.ts
+++ ProAnimal/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
   production: true
 };
+const firebaseConfig = {
+  apiKey: 'AIzaSyCseLjrb13PC0EdpBplM56-JE2orA-M0Ls',
+  authDomain: 'proanimal-108e2.firebaseapp.com',
+  databaseURL: 'https://proanimal-108e2.firebaseio.com',
+  projectId: 'proanimal-108e2',
+  storageBucket: 'proanimal-108e2.appspot.com',
+  messagingSenderId: '378439661',
+  appId: '1:378439661:web:cd97d9302fc9e4440349e4',
+  measurementId: 'G-QL9104ZFQ4'
+};
+
